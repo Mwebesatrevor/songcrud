65099fb6d0c20399d6300d5b4832183c96f66481
Great Dane <trevormwebesa@gmail.com>
2022-10-26 17:50:14 +0300

    made the models and registered them in admin

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from .models import Artiste, Song, Lyric
 
+admin.site.register(Artiste)# Registered the Artiste model
+admin.site.register(Song)# Registered the Song model
+admin.site.register(Lyric)# Registered the Lyric model
 # Register your models here.
+
 
 
 
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Artiste(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+    
+class Song(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    date_released = models.DateField()
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+class Lyric(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
